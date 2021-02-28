2271801d273ba296cf4d4c483db727acb7aac08e
nightingale38 <romain.ageron@student-cs.fr>
2021-02-28 10:51:35 +0100

    bug fix + nouvelles options pour charger les partitions

--- extension/schur_5/load_partitions.py
+++ extension/schur_5/load_partitions.py
@@ -1,6 +1,7 @@
 import heapq
 import os
 import pickle
+import random
 import tqdm
 
 
@@ -22,15 +23,24 @@ def read_file(filename):
     return map(from_int, partitions)
 
 
-def load_partitions():
+def load_partitions(n_groups=None, take_first=True):
     _, _, groups = next(os.walk("./partitions/all_partitions/"))
+
+    if not take_first:
+        random.shuffle(groups)
+    if n_groups is None:
+        n_groups = len(groups)
+    if isinstance(n_groups, float):
+        n_groups = round(n_groups * len(groups))
+    groups = groups[:n_groups]
+
     for group in tqdm.tqdm(groups):
         for partition in read_file(group):
             yield partition
 
 
 def select_partitions(necessary_condition=None, fitness_function=None,
-                      max_size=10_000):
+                      max_size=None, n_groups=None, take_first=True):
     if necessary_condition is None:
         necessary_condition = lambda _: True
     if fitness_function is None:
@@ -38,11 +48,13 @@ def select_partitions(necessary_condition=None, fitness_function=None,
         stop_early = True
     else:
         stop_early = False
+    if max_size is None:
+        max_size = 3_000_000_000
 
     heap = []
     counter = 0
 
-    for partition in load_partitions():
+    for partition in load_partitions(n_groups, take_first):
         if not necessary_condition(partition):
             continue
 
@@ -57,9 +69,9 @@ def select_partitions(necessary_condition=None, fitness_function=None,
                 break
             heapq.heappushpop(heap, item)
 
-    partition = [None] * len(heap)
+    partitions = [None] * len(heap)
     i = 1
     while heap:
-        partitions[-i] = heapq.heappop(heap)
+        partitions[-i] = heapq.heappop(heap)[-1]
         i += 1
     return partitions
