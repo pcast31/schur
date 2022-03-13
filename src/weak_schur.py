9be268369a5c470bb7aefcf8f307da8925013ed4
sharknaedo <prasanna.maddila@student-cs.fr>
2022-03-13 14:04:57 +0100

    src/weak_schur: added fitness single threaded

--- src/weak_schur.py
+++ src/weak_schur.py
@@ -5,6 +5,7 @@ with a view to parallelize it someday.
 """
 import json
 import numpy as np
+from copy import deepcopy
 
 
 def verify_partition(partition: list) -> bool:
@@ -51,7 +52,7 @@ def fitness(partition: list) -> int:
 
     Returns
     -------
-            : int
+    fitness_num  : int
             The fitness score of the partition i.e. the number of pairs
             violating the sum-free property.
     """
@@ -68,22 +69,60 @@ def fitness(partition: list) -> int:
     return fitness_sum
 
 
-def generate_partition(partition: list, num_colors: int, num_to_add: int) -> list:
-    len_partition = len(partition)
-    if num_colors < len_partition:
-        # Don't need to do anything if we're looking
-        # for a smaller partition than we have.
-        return partition
-    # Add space for new partitions
-    for i in range(1, num_colors - len_partition + 1):
-        partition[f"E{len_partition + i}"] = []
+def generate_partition(num_colors: int, n: int, choice=np.argmin) -> tuple:
+    """Function to generate a weakly sum-free partition
+    of the first `n` numbers into `num_colors` colors
 
-    print(partition)
-    return partition
+    The function does not guarantee a true partition, but it
+    does guarantee to choose the one that minimises fitness.
+
+    Parameters
+    ----------
+    num_colors : int
+        the number of colors to partition our numbers into
+    n : int
+        The maximum number of ints to add to the partition,
+        starting from 1
+    choice: function
+        The default choice function to use when taking
+        the best solution from each iteration. It must have
+        the signature:
+            choice: list(int) -> int
+
+
+    Returns
+    -------
+    (best_solution, fitness_score)
+        the tuple of the best solution and the associated fitness.
+    """
+    num = 0
+    best_solution = [[] for _ in range(num_colors)]
+    solutions = [[] for _ in range(num_colors)]
+    fitness_solutions = np.zeros(shape=(num_colors, 1), dtype=int)
+    while num < n:
+        # Add integers to partitions as long
+        # as there are numbers to add.
+        num = num + 1
+        for idx in range(num_colors):
+            # create the new candidate solution
+            temp = deepcopy(best_solution)
+            temp[idx].append(num)
+
+            # add it to the list
+            solutions[idx] = temp
+            fitness_solutions[idx] = fitness(temp)
+
+        # Now, we choose the best one
+        best_solution = solutions[choice(fitness_solutions)]
+
+    print(f"Partition: {best_solution}")
+    print(f"Fitnesses: {fitness_solutions}")
+    return best_solution
 
 
 if __name__ == "__main__":
     with open("data/partition6.json", "r", encoding="utf-8") as fp:
         partition6 = json.loads(fp.read())
     print(f"Fitness of the partition = {fitness(partition6)}")
-    generate_partition(partition6, 8)
+    result = generate_partition(num_colors=5, n=100)
+    # print(f"Partition generated: {result}")
